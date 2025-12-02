b581987987a7915e57ca881606fa5d47ae7d31bf
IHateSpaghettiCode <jftangarifem@gmail.com>
2025-12-01 23:46:42 -0500

    Delete Documents directory

--- Documents/TECHOMATCH_ADAPTADO_A_REACT/src/pages/UserZonePage.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import UserZone from "../componentes/UserZone";
-import "../styles/User.module.css";
-
-function UserZonePage() {
-  return <UserZone />;
-}
-
-export default UserZonePage;
